b90be018f3851e933d1d2bb8240309fd8ba1cdd5
CarlosEZP <carloszanca03@gmail.com>
2025-09-02 22:30:33 -0300

    Add new controller CategoryController with getAllCategories method

---
+++ back-end/src/main/java/dev/carlosezp/ecommerce/controllers/CategoryController.java
@@ -0,0 +1,16 @@
+package dev.carlosezp.ecommerce.controllers;
+
+import dev.carlosezp.ecommerce.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController()
+public class CategoryController {
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return new ArrayList<Category>();
+    }
+}
