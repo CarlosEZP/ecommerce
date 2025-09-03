af2339b9e613d37411e30e7c30b188fe01c68ebd
CarlosEZP <carloszanca03@gmail.com>
2025-09-02 22:36:12 -0300

    Add new method "add" to controller and a list to mock/test API

--- back-end/src/main/java/dev/carlosezp/ecommerce/controllers/CategoryController.java
+++ back-end/src/main/java/dev/carlosezp/ecommerce/controllers/CategoryController.java
@@ -2,6 +2,8 @@ package dev.carlosezp.ecommerce.controllers;
 
 import dev.carlosezp.ecommerce.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,8 +11,16 @@ import java.util.List;
 
 @RestController()
 public class CategoryController {
+    List<Category> categories = new ArrayList<Category>();
+
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return new ArrayList<Category>();
+        return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String add(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully";
     }
 }
