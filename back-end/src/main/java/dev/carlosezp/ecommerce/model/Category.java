bad5c80948a9470b87e163515262173ffc022060
CarlosEZP <carloszanca03@gmail.com>
2025-09-02 22:29:43 -0300

    Add new model Category

---
+++ back-end/src/main/java/dev/carlosezp/ecommerce/model/Category.java
@@ -0,0 +1,23 @@
+package dev.carlosezp.ecommerce.model;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
