fd2e0565bcf4cffc9795edc3753857dbcd105727
CarlosEZP <carloszanca03@gmail.com>
2025-09-04 14:11:15 -0300

    Refactor to add CategoryRepository and to be use in CategoryServiceImpl

--- back-end/src/main/java/dev/carlosezp/ecommerce/services/CategoryServiceImpl.java
+++ back-end/src/main/java/dev/carlosezp/ecommerce/services/CategoryServiceImpl.java
@@ -2,6 +2,8 @@ package dev.carlosezp.ecommerce.services;
 
 import dev.carlosezp.ecommerce.interfaces.CategoryService;
 import dev.carlosezp.ecommerce.models.Category;
+import dev.carlosezp.ecommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,35 +13,30 @@ import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    List<Category> categories = new ArrayList<Category>();
-    Long nextId = 0L;
+    @Autowired
+    CategoryRepository categoryRepository;
     @Override
     public List<Category> getAll() {
-        return categories;
+        return categoryRepository.findAll();
     }
     @Override
     public String add(Category category) {
-        category.setId(++nextId);
-        categories.add(category);
+        categoryRepository.save(category);
         return "Add new " + category.getName() + " category successfully";
     }
     @Override
     public String delete(Long categoryId){
-        Category category = categories.stream()
-                .filter(x -> x.getId().equals(categoryId))
-                .findFirst()
+        Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not Found!"));
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Delete successfully";
     }
     @Override
     public String update(Category category) {
-        Category categoryUpdated = categories.stream()
-                .filter(x -> x.getId().equals(category.getId()))
-                .findFirst()
+        categoryRepository.findById(category.getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not Found!"));
-        categoryUpdated.setName(category.getName());
-        return "Update category successfully: " + categoryUpdated;
+        categoryRepository.save(category);
+        return "Update category successfully: " + category;
     }
 
 }
